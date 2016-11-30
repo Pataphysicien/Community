fe7544b0ba60b20f9e7921a21e1554cc424cfc9c
Pataphysicien <ma.coutanceau@gmail.com>
2016-12-01 00:08:32 +0100

    Adding F# exercices and notes from course, about automatic currying

---
+++ uid-01-begin-fsharp/1.2.PlayingWithFunctions_AutomaticCurrying.fsx
@@ -0,0 +1,22 @@
+﻿let ageOfBob = 2
+let ageOfSam = 4
+
+//Remember, F# function take only one argument,
+//...but this one looks like having two :
+let addAges bob sam =
+    bob + sam
+
+let sumOfAges1 = addAges ageOfBob ageOfSam;
+printfn "%A" sumOfAges1
+
+//  Let's take a look a the output signature of 'addAges':
+//      val addAges : bob:int -> sam:int -> int
+//  it's actually a function returning a function returning an int !
+//  F# has rewritten the function, we could also have write it like this :
+let addAgesV2 bob =
+    let addAgesV2_Sub sam =
+        bob + sam
+    addAgesV2_Sub
+
+let sumOfAges2 = addAgesV2 ageOfBob ageOfSam
+printfn "%A" sumOfAges2
---
+++ uid-01-begin-fsharp/uid-01-begin-fsharp/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace uid_01_begin_fsharp.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("uid-01-begin-fsharp")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("uid-01-begin-fsharp")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("57da8564-60f2-4bbd-926b-8bc354cfba88")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
