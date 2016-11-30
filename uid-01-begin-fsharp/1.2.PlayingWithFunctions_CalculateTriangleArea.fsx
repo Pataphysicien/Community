e6c0fa42585ccd5e737bb3d3a4b2337742928519
Pataphysicien <ma.coutanceau@gmail.com>
2016-12-01 00:31:20 +0100

    adding exercice, calculating triangle area

---
+++ uid-01-begin-fsharp/1.2.PlayingWithFunctions_CalculateTriangleArea.fsx
@@ -0,0 +1,12 @@
+﻿let triangleV = (4.0 , 5.0 , 3.0)
+
+let semiperimeterFn pTriangle =
+    let sideA,sideB,sideC = pTriangle
+    (0.5*(sideA + sideB + sideC))
+
+let triangleAreaFn pTriangle =
+    let sideA, sideB, sideC = pTriangle
+    let s = semiperimeterFn pTriangle
+    sqrt(s*(s-sideA)*(s-sideB)*(s-sideC))
+
+triangleAreaFn triangleV
